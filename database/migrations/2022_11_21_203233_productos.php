fadc696f1f75cf08d5b254d6851109a198ea8fa0
victormv08 <vmarulandav@unal.edu.co>
2022-11-25 15:56:25 -0500

    Se actualiza la rama de desarrolloVictor

---
+++ database/migrations/2022_11_21_203233_productos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Productos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_producto');
+            $table->integer('cantidad_producto');
+            $table->integer('precio_producto');
+            $table->string('descripcion_producto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_22_002357_ventas.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Ventas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->id();
+            $table->integer('precio_venta');
+            $table->unsignedBigInteger('id_usuario');
+            $table->foreign("id_usuario")
+                ->references("id")
+                ->on("usuario")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->unsignedBigInteger('id_producto');
+            $table->foreign("id_producto")
+                ->references("id")
+                ->on("productos")
+                ->onDelete("cascade")
+                ->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
